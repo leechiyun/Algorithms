b54382bf6a0d756cce0bf5ccfeb41f710c81b977
joker123 <56662119+leechiyun@users.noreply.github.com>
2023-07-14 21:42:15 +0900

    [Bronze V] Title: 문자열, Time: 116 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// Input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			String str = br.readLine();
+			System.out.println(String.valueOf(str.charAt(0)) + String.valueOf(str.charAt(str.length()-1)));
+		}
+	}
+}
