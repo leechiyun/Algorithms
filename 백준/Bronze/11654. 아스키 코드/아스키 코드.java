b042863f76f913b49b73c41021e1921311eb185b
joker123 <56662119+leechiyun@users.noreply.github.com>
2023-07-14 21:47:45 +0900

    [Bronze V] Title: 아스키 코드, Time: 128 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// Input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		char c = br.readLine().charAt(0);
+		System.out.println((int)c);
+	}
+}
