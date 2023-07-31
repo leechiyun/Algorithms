928dd321dc678396449dfe8490da0e7f07c61b9e
joker123 <56662119+leechiyun@users.noreply.github.com>
2023-07-31 16:31:50 +0900

    [Bronze II] Title: 벌집, Time: 140 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int mathTemp = 1;
+        int count = 1;
+        while (N > mathTemp) {
+            mathTemp += 6 * count;
+            count++;
+        }
+
+        System.out.println(count);
+    }
+
+}
