4843baa0cc01637475eed6ee41a09bf9d2cc6b18
joker123 <56662119+leechiyun@users.noreply.github.com>
2023-09-28 20:30:44 +0900

    [Silver V] Title: 다리 놓기, Time: 132 ms, Memory: 15064 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb =new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            sb.append(calculateCombination(M, N)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+    public static int calculateCombination(int n, int r) {
+        int[][] dp = new int[n + 1][r + 1];
+
+        // 모든 경우의 수 초기화
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= Math.min(i, r); j++) {
+                if (j == 0 || j == i) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][r];
+    }
+}
+
