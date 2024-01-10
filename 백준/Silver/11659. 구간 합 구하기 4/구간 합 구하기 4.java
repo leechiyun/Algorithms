4487c6debf173def5849e444ecfd6b006d6718d4
joker123 <56662119+leechiyun@users.noreply.github.com>
2024-01-10 10:05:55 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1460 ms, Memory: 59244 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,35 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken()); //첫째 줄에 수의 개수
+        int m = Integer.parseInt(st.nextToken()); //합을 구해야하는 횟수
 
-        // list: 구간합
-        List<Integer> list = new ArrayList<>();
-        st = new StringTokenizer(br.readLine(), " ");
-        list.add(0);
-        for (int i = 1; i <= N; i++) {
-            list.add(list.get(i - 1) + Integer.parseInt(st.nextToken()));
+        int[] arr = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<n+1; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            arr[i] = arr[i-1] + tmp;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-
-            sb.append(list.get(end) - list.get(start - 1)).append("\n");
+            
+            System.out.println(arr[end] -  arr[start-1]);
         }
-
-        System.out.println(sb);
     }
 }
-
