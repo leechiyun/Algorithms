dcf2a3622607762c2b6124844fb9e62807ec8fa3
joker123 <56662119+leechiyun@users.noreply.github.com>
2024-04-13 12:58:52 +0900

    [Gold IV] Title: 여왕벌, Time: 876 ms, Memory: 114416 KB, Score: 83 point -BaekjoonHub

---
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int map[][];
+    static List<Node> nodes;
+    static int nums[] = {0, 1, 2};
+
+    static class Node {
+        int x, y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+        for(int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                map[i][j] = 1;
+            }
+        }
+
+        nodes = new ArrayList<>();
+        for (int i = N-1; i >= 0; i--) {
+            nodes.add(new Node(i, 0));
+        }
+        for (int j = 1; j < N; j++) {
+            nodes.add(new Node(0, j));
+        }
+
+        int cnt[] = new int[3];
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < cnt.length; i++) {
+                cnt[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int idx = 0;
+            for (int c = 0; c < cnt.length; c++) {
+                for (int i = idx; i < idx + cnt[c]; i++) {
+                    Node cur = nodes.get(i);
+
+                    map[cur.x][cur.y] += nums[c];
+                }
+
+                idx += cnt[c];
+            }
+        }
+
+        // 돌아가며 왼쪽, 왼쪽위, 위를 비교하고 큰 값저장
+        int dx[] = {0, -1, -1};
+        int dy[] = {-1, -1, 0};
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                for (int d = 0; d < dx.length; d++) {
+                    int nextX = i + dx[d];
+                    int nextY = j + dy[d];
+
+                    if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < N) {
+                        map[i][j] = Math.max(map[i][j], map[nextX][nextY]);
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(map[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
