a33af81c1766e5be731ee38de9bcd858d2889805
joker123 <56662119+leechiyun@users.noreply.github.com>
2024-05-28 15:49:22 +0900

    [level 2] Title: 전화번호 목록, Time: 120.19 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,19 +3,25 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         Map<String, Integer> map = new HashMap<>();
+        Set<Integer> lenSet = new HashSet<>();
         for(String phone: phone_book) {
-            map.put(phone, map.getOrDefault(phone, 0) + 1);
+            map.put(phone, phone.length());
+            lenSet.add(phone.length());
         }
-
+        
+        // 확인
         for(String phone: phone_book) {
-            for(int i = 0; i < phone.length(); i++){
-                if(map.containsKey(phone.substring(0, i))){
-                    return false;
+            for(int len: lenSet) {
+                if(len < phone.length()) {
+                    String str = phone.substring(0, len);
+                    
+                    if(map.containsKey(str)){
+                        return false;
+                    }
                 }
             }
         }
 
-        boolean answer = true;
-        return answer;
+        return true;
     }
 }
