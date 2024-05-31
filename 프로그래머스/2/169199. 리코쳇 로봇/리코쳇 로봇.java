101bac50091ab6796874da639ecd751d64b56eb9
joker123 <56662119+leechiyun@users.noreply.github.com>
2024-06-01 01:13:30 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.72 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,71 +1,78 @@
 import java.util.*;
 
 class Solution {
-    // 상, 하, 좌, 우
-    static int dx[] = new int[] {-1, 1, 0, 0};
-    static int dy[] = new int[] {0, 0, -1, 1};
+    static int dx[] = {-1, 1, 0, 0};
+    static int dy[] = {0, 0, -1, 1};
     
-    static boolean visited[][];
-    static int N = 0;
-    static int M = 0;
+    static char map[][];
+    static int N, M;
     
     static class Node {
-        int x;
-        int y;
-        int count;
+        int x, y;
+        int cnt;
         
-        public Node(int x, int y, int count) {
+        public Node(int x, int y, int cnt) {
             this.x = x;
             this.y = y;
-            this.count = count;
+            this.cnt = cnt;
         }
     }
     
     public int solution(String[] board) {
         N = board.length;
         M = board[0].length();
-        visited = new boolean[N][M];
         
-        Queue<Node> queue = new ArrayDeque<>();
-        for(int i = 0; i < N; i++) {
+        Node start = null;
+        Node end = null;
+        map = new char[N][M];
+        for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++) {
-                if(board[i].charAt(j) == 'R') {
-                    queue.offer(new Node(i, j, 0));
-                    visited[i][j] = true;
+                char c = board[i].charAt(j);
+                
+                if(c == 'R') {
+                    start = new Node(i, j, 0);
                 }
+                if(c == 'G') {
+                    end = new Node(i, j, 0);
+                }
+                
+                map[i][j] = c;
             }
         }
         
-        List<Integer> results = new ArrayList<>();
-        while(!queue.isEmpty()){
-            Node curNode = queue.poll();
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(start);
+        boolean visited[][] = new boolean[N][M];
+        visited[start.x][start.y] = true;
+        
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
             
-            if(board[curNode.x].charAt(curNode.y) == 'G') {
-                results.add(curNode.count);
+            if(cur.x == end.x && cur.y == end.y) {
+                return cur.cnt;
             }
             
-            for(int i = 0; i < dx.length; i++){
-                int nextX = curNode.x + dx[i];
-                int nextY = curNode.y + dy[i];
+            for(int d = 0; d < dx.length; d++) {
+                // 갈 수 있는 곳까지 간다.
+                int nextX = cur.x + dx[d];
+                int nextY = cur.y + dy[d];
                 
-                while(nextX >= 0 && nextX < N && nextY >= 0 && nextY < M 
-                  && board[nextX].charAt(nextY) != 'D') {
-                    nextX += dx[i];
-                    nextY += dy[i];
+                while(nextX >= 0 && nextX < N && nextY >= 0 && nextY < M
+                        && map[nextX][nextY] != 'D') {
+                    nextX += dx[d];
+                    nextY += dy[d];
                 }
                 
-                nextX -= dx[i];
-                nextY -= dy[i];
+                nextX -= dx[d];
+                nextY -= dy[d];
+                
                 if(!visited[nextX][nextY]) {
-                    queue.offer(new Node(nextX, nextY, curNode.count+1));
+                    q.offer(new Node(nextX, nextY, cur.cnt + 1));
                     visited[nextX][nextY] = true;
                 }
             }
         }
-        if(results.isEmpty()) {
-            return -1;
-        }
         
-        return Collections.min(results);
+        return -1;
     }
 }
