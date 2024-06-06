35d671779bc4e8889f0964825a5b73028e6b8147
joker123 <56662119+leechiyun@users.noreply.github.com>
2024-06-06 10:49:14 +0900

    [level 2] Title: 미로 탈출, Time: 0.25 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 class Solution {
-    static boolean visitedL[][];
-    static boolean visitedM[][];
-    static int N;
-    static int M;
+    
+    static char map[][];
+    static int N, M;
     
     static int dx[] = {-1, 1, 0, 0};
     static int dy[] = {0, 0, -1, 1};
     
     static class Node {
-        int x;
-        int y;
-        int count;
+        int x, y;
+        int cnt;
+        int key;       // 0: 레버 열지 못함, 1: 레버 열음
         
-        public Node(int x, int y, int count) {
+        public Node(int x, int y, int cnt, int key) {
             this.x = x;
             this.y = y;
-            this.count = count;
+            this.cnt = cnt;
+            this.key = key;
         }
     }
     
@@ -25,85 +25,55 @@ class Solution {
         N = maps.length;
         M = maps[0].length();
         
-        visitedL = new boolean[N][M];
-        visitedM = new boolean[N][M];
-        
-        // 시작 지점 찾기
-        int startX = 0;
-        int startY = 0;
-        boolean startFlag = false;
+        Node start = null;
+        Node end = null;
+        map = new char[N][M];
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < M; j++) {
-                if(maps[i].charAt(j) == 'S') {
-                    startX = i;
-                    startY = j;
-                    startFlag = true;
-                }
-            }
-            if(startFlag) {
-                break;
-            }
-        }
-        
-        // 레버 찾기
-        List<Node> resultL = new ArrayList<>();
-        
-        Queue<Node> queue = new ArrayDeque<>();
-        queue.offer(new Node(startX, startY, 0));
-        while(!queue.isEmpty()) {
-            Node curNode = queue.poll();
-            
-            if(maps[curNode.x].charAt(curNode.y) == 'L') {
-                resultL.add(new Node(curNode.x, curNode.y, curNode.count));
-            }
-            
-            for(int i = 0; i < dx.length; i++) {
-                int nextX = curNode.x + dx[i];
-                int nextY = curNode.y + dy[i];
+                char c = maps[i].charAt(j);
                 
-                if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < M && 
-                   !visitedL[nextX][nextY] && maps[nextX].charAt(nextY) != 'X') {
-                    queue.offer(new Node(nextX, nextY, curNode.count + 1));
-                    visitedL[nextX][nextY] = true;
+                if(c == 'S') {
+                    start = new Node(i, j, 0, 0);
                 }
-            }
-        }
-        
-        // 레버를 찾지 못한 경우, return -1
-        if(resultL.isEmpty()) {
-            return -1;
-        }
-        
-        // 출구 찾기
-        Node minNode = Collections.min(resultL, (a, b) -> a.count - b.count);
-        System.out.println(minNode.count);
-        List<Integer> results = new ArrayList<>();
-        
-        queue = new ArrayDeque<>();
-        queue.offer(new Node(minNode.x, minNode.y, minNode.count));
-        while(!queue.isEmpty()) {
-            Node curNode = queue.poll();
-            
-            if(maps[curNode.x].charAt(curNode.y) == 'E') {
-                results.add(curNode.count);
-            }
-            
-            for(int i = 0; i < dx.length; i++) {
-                int nextX = curNode.x + dx[i];
-                int nextY = curNode.y + dy[i];
+                if(c == 'E') {
+                    end = new Node(i, j, 0, 0);
+                }
                 
-                if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < M && 
-                   !visitedM[nextX][nextY] && maps[nextX].charAt(nextY) != 'X') {
-                    queue.offer(new Node(nextX, nextY, curNode.count + 1));
-                    visitedM[nextX][nextY] = true;
-                }
+                map[i][j] = c;
             }
         }
         
-        if(results.isEmpty()) {
-            return -1;
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(start);
+        boolean visited[][][] = new boolean[2][N][M];
+        visited[0][start.x][start.y] = true;
+        
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            
+            if(cur.x == end.x && cur.y == end.y && cur.key == 1) {
+                return cur.cnt;
+            }
+            
+            for(int d = 0; d < dx.length; d++) {
+                int nextX = cur.x + dx[d];
+                int nextY = cur.y + dy[d];
+                
+                if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < M
+                    && !visited[cur.key][nextX][nextY] && map[nextX][nextY] != 'X') {
+                    // 레버인 경우
+                    if(map[nextX][nextY] == 'L') {
+                        q.offer(new Node(nextX, nextY, cur.cnt + 1, 1));
+                        visited[1][nextX][nextY] = true;
+                    } else {
+                        q.offer(new Node(nextX, nextY, cur.cnt + 1, cur.key));
+                        visited[cur.key][nextX][nextY] = true;
+                    }
+                    
+                } 
+            }
         }
-
-        return Collections.min(results);
+        
+        return -1;
     }
 }
