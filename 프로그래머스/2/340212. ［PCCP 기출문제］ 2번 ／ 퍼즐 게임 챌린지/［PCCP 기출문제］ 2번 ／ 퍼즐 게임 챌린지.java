9d6ea19597d4bba52a568f049799e47467a5fc8d
joker123 <56662119+leechiyun@users.noreply.github.com>
2025-08-24 21:39:28 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 48.52 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,60 +1,43 @@
 class Solution {
-    static int max = Integer.MIN_VALUE;
-    static int min = 1;
-    
-    static int ds[];
-    static int ts[];
-    static long l;
-    
     public int solution(int[] diffs, int[] times, long limit) {
-        int answer = 0;
-        ds = diffs;
-        ts = times;
-        l = limit;
+        int left = 1;
+        int right = 100000;
+        int level = right;
         
-        for(int diff: diffs) {
-            max = Math.max(max, diff);
-        }
+        int len = diffs.length;
         
-        // 이분 탐색
-        int mid = 0;
-        long n;
-        while(min <= max) {
-            mid = (min + max) / 2;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long sum = 0;
             
-            n = calc(mid);
+            for(int i = 0; i < len; i++) {
+                int d = diffs[i];
+                int pt = 0;
+                if(i > 0) {
+                    pt = times[i-1];
+                }
+                int ct = times[i];
+                
+                sum += calc(d, pt, ct, mid);
+            }
             
-            // limit 전이면 왼쪽
-            if(n > limit) {
-                min = mid + 1;
-            } else{
-                max = mid - 1;
-                answer = mid;
+            if(sum <= limit){
+                level = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         
-        return answer;
+        return level;
     }
     
-    public long calc(int level) {
-        int len = ds.length;
-        long sum = 0;
-        
-        int prev = 0;
-        int d;
-        int t;
-        for(int i = 0; i < len; i++) {
-            d = ds[i];
-            t = ts[i];
-            prev = i > 0 ? ts[i-1]:0;
-            
-            if(d <= level) {
-                sum += t;
-            } else {
-                sum += (d - level) * (t + prev) + t;
-            }
+    public int calc(int diff, int prev, int cur, int level) {
+        if(level >= diff) {
+            return cur;
         }
         
-        return sum;
+        int n = diff - level;
+        return cur + ((cur + prev) * n);
     }
 }
